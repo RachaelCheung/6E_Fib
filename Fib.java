7ebb1871c42eb5be8a16d882cc4f65a73472e3fb
Rachael Cheung <rcheung00@stuy.edu>
2019-05-13 22:52:34 -0400

    final version

--- Fib.java
+++ Fib.java
@@ -11,10 +11,16 @@ public class Fib {
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_recurrence( int n) {
+	if (n == 1 || n == 2)
+		return 1;
+	else if (n < 1)
+		return 0;
+	else
+		return fib_recurrence(n-1) + fib_recurrence(n-2);
     }
      /* These are class / instance methods because...
+		UserOfFib does not create an instance of this class, and directly invokes the method.
     */
 
 
@@ -22,18 +28,31 @@ public class Fib {
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_grade8( int n) {
+        int num0 = 0; // pre-number
+		int num1 = 1; // first number of fibonacci sequence
+		int temp;
+		int currentFib = 1; // "n", or the number of num1
+		if (n == 0) return 0;
+		
+		while (currentFib < n){
+			temp = num0;
+			num0= num1;
+			num1 = temp + num1;
+			currentFib++;
+		}
+		return num1;
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be n
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count 
+	   the number of times currentFib is incremented
        
        Then cost of the the recurrence algorithm
-       grows ?? 
+       grows proportional to n
        as the size of the problem increases,
-       because ??
+       because as "n" increases, two more invocations of the method are needed.  
      */
 
 
@@ -43,17 +62,21 @@ public class Fib {
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
-        return -2; // invalid Fibonacci number
+    public static double fib_Binet( int n) {
+		double phi = (1 + Math.sqrt(5)) / 2;
+		double psi = (1 - Math.sqrt(5)) / 2;
+        return ( Math.pow( phi , n) 
+				-  Math.pow( psi, n) ) 
+				/ Math.sqrt(5);
     }
     /* Time complexity:
-       Consider the size of the problem to be ...
+       Consider the size of the problem to be n
        
-       As the proxy for the time required, count...
+       As the proxy for the time required, count the (?)
        
        Then cost of the the recurrence algorithm
-       grows ?? 
+       remains constant	   
        as the size of the problem increases,
-       because ??
+       because the same number of operations are being performed is not dependent on n.
      */
 }
